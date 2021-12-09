dda9714e897b5a1aeb4b3bb6056a7cbd8fc0e35a
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2021-12-09 14:25:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const fetch = require("node-fetch");
+const { Session, Cloud } = require('scratchcloud');
+const s2n = require('string2num');
+var Vibrant = require('node-vibrant');
+let at = '';
+var Scratch = require('scratch-api');
 
+Scratch.UserSession.create(process.env.USERNAME, process.env.PASSWORD, function(err, user) {
+  user.cloudSession(process.env.PROJECT_ID, function(err, cloud) {
+
+
+    })
+    
+  cloud.set(c('pleaseRefresh'), 200);
